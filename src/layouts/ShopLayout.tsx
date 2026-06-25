c8950d193e936ca5ee1b6b872e15f5d7da983c91
Albert <albert@sirvelia.com>
2026-06-25 17:16:39 +0200

    refactor: reduce routes to checkout + thank-you

--- src/layouts/ShopLayout.tsx
+++ src/layouts/ShopLayout.tsx
@@ -2,16 +2,11 @@ import { Outlet } from "react-router";
 
 import { SiteFooter } from "@/components/site";
 import { UrgencyRail } from "@/components/site/UrgencyRail";
-import { LoggedInBar } from "@/components/site/LoggedInBar";
-import { useAuth } from "@/hooks/useAuth";
 
 export function ShopLayout() {
-  const { status } = useAuth();
-
   return (
     <div className="min-h-dvh flex flex-col bg-sand text-ink">
       <UrgencyRail />
-      {status === "authenticated" && <LoggedInBar />}
       <div className="flex-1">
         <Outlet />
       </div>
